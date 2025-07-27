fb0227432510ab696831b42d27e3569ebdf948fb
deimos <deimoshall@gmail.com>
2025-07-27 09:20:46 -0600

    feat: create project

---
+++ composeApp/src/androidMain/kotlin/dev/deimos/wordcounter/Platform.android.kt
@@ -0,0 +1,9 @@
+package dev.deimos.wordcounter
+
+import android.os.Build
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
---
+++ composeApp/src/desktopMain/kotlin/dev/deimos/wordcounter/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package dev.deimos.wordcounter
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
