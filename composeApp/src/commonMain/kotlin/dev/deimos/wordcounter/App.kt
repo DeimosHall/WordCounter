f0f90ed91750ecd71848662e03dae45edface3e5
deimos <deimoshall@gmail.com>
2025-08-16 13:40:38 -0600

    feat: count characters

--- composeApp/src/commonMain/kotlin/dev/deimos/wordcounter/App.kt
+++ composeApp/src/commonMain/kotlin/dev/deimos/wordcounter/App.kt
@@ -2,8 +2,11 @@ package dev.deimos.wordcounter
 
 import CustomTextField
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,15 +15,20 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -31,27 +39,64 @@ import wordcounter.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        // TODO: save it even on change of portrait or landscape modes
-        var textState by remember { mutableStateOf("") }
+        var textState by rememberSaveable() { mutableStateOf("") }
+        var textLength by rememberSaveable() { mutableStateOf(0) }
+
         Column(
             modifier = Modifier
                 .safeContentPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            CustomTextField(
-                value = textState,
-                onValueChanged = { textState = it },
-                hintText = "Type text here",
-                maxLines = 10,
+            Box(
+                contentAlignment = Alignment.Center,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clip(RoundedCornerShape(5.dp))
-//                    .background(MaterialTheme.colorScheme.surface)
-                    .background(Color.LightGray)
                     .padding(16.dp)
-            )
+            ) {
+                Text(
+                    text = "Word Counter",
+                    fontSize = 25.sp,
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text(text = "Text Input", modifier = Modifier.padding(bottom = 10.dp))
+                CustomTextField(
+                    value = textState,
+                    onValueChanged = { textState = it },
+                    hintText = "Type text here",
+                    maxLines = 10,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(12.dp))
+//                    .background(MaterialTheme.colorScheme.surface)
+                        .background(Color.LightGray)
+                        .padding(16.dp)
+                )
+                Text(text = "Stats", modifier = Modifier.padding(top = 10.dp, bottom = 10.dp))
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(12.dp))
+                            .border(border = BorderStroke(
+                                1.dp,
+                                color = Color.LightGray
+                            )
+                        )
+                    ) {
+                        Column(modifier = Modifier.padding(30.dp)) {
+                            Text(text = "Characters")
+                            Text(text = textState.length.toString())
+                        }
+                    }
+                }
+            }
         }
     }
 }
