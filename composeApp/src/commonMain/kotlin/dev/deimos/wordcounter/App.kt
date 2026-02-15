b66382e3bab89bb3c6bae600e939f7f60f1ba516
deimos <deimoshall.dev@proton.me>
2026-02-15 10:52:38 -0600

    feat: add clear button

--- composeApp/src/commonMain/kotlin/dev/deimos/wordcounter/App.kt
+++ composeApp/src/commonMain/kotlin/dev/deimos/wordcounter/App.kt
@@ -6,11 +6,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
@@ -40,6 +45,7 @@ fun App() {
 
     MaterialTheme {
         var textState by rememberSaveable() { mutableStateOf("") }
+        val scrollState = rememberScrollState()
 
         Column(
             modifier = Modifier
@@ -63,7 +69,10 @@ fun App() {
             }
             Column(
                 verticalArrangement = Arrangement.spacedBy(16.dp),
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .verticalScroll(scrollState)
             ) {
                 Text(text = "Text Input")
                 AreaTextField(
@@ -72,26 +81,34 @@ fun App() {
                     hintText = "Tab to paste",
                 )
                 Text(text = "Stats")
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-                    modifier = Modifier.fillMaxWidth()
+                Column(
+                    modifier = Modifier.fillMaxWidth().height(240.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
                 ) {
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        StatCard(
+                            "Characters",
+                            textState.length.toString(),
+                            modifier = Modifier.weight(1f)
+                        )
+                        StatCard(
+                            "Words",
+                            getWordsCounting(textState).toString(),
+                            modifier = Modifier.weight(1f)
+                        )
+                    }
                     StatCard(
-                        "Characters",
-                        textState.length.toString(),
-                        modifier = Modifier.weight(1f)
-                    )
-                    StatCard(
-                        "Words",
-                        getWordsCounting(textState).toString(),
+                        "Lines",
+                        getLinesCounting(textState).toString(),
                         modifier = Modifier.weight(1f)
                     )
                 }
-                StatCard(
-                    "Lines",
-                    getLinesCounting(textState).toString(),
-                    modifier = Modifier.weight(1f)
-                )
+            }
+            Button(onClick = { textState = "" }, modifier = Modifier.fillMaxWidth()) {
+                Text("Clear")
             }
         }
     }
