1d5ff15cd825c369541c6fb1da1e089965254e40
deimos <deimoshall.dev@proton.me>
2026-02-20 18:10:55 -0600

    fix: center content

--- composeApp/src/commonMain/kotlin/dev/deimos/wordcounter/App.kt
+++ composeApp/src/commonMain/kotlin/dev/deimos/wordcounter/App.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -47,70 +49,74 @@ fun App(renderTitle: Boolean = true) {
         var textState by rememberSaveable() { mutableStateOf("") }
         val scrollState = rememberScrollState()
 
-        Column(
-            modifier = Modifier
-                .safeContentPadding()
-                .fillMaxSize()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            if (renderTitle) {
-                Box(
-                    contentAlignment = Alignment.Center,
+        Box(modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center) {
+            Column(
+                modifier = Modifier
+                    .safeContentPadding()
+                    .fillMaxHeight()
+                    .widthIn(max = 1000.dp)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                if (renderTitle) {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ) {
+                        Text(
+                            text = "Word Counter",
+                            fontSize = 25.sp,
+                            fontStyle = FontStyle.Normal,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(16.dp)
+                        .weight(1f)
+                        .verticalScroll(scrollState)
                 ) {
-                    Text(
-                        text = "Word Counter",
-                        fontSize = 25.sp,
-                        fontStyle = FontStyle.Normal,
-                        fontWeight = FontWeight.Bold
+                    Text(text = "Text Input")
+                    AreaTextField(
+                        value = textState,
+                        onValueChanged = { textState = it },
+                        hintText = "Text",
                     )
-                }
-            }
-            Column(
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-                    .verticalScroll(scrollState)
-            ) {
-                Text(text = "Text Input")
-                AreaTextField(
-                    value = textState,
-                    onValueChanged = { textState = it },
-                    hintText = "Tab to paste",
-                )
-                Text(text = "Stats")
-                Column(
-                    modifier = Modifier.fillMaxWidth().height(240.dp),
-                    verticalArrangement = Arrangement.spacedBy(16.dp)
-                ) {
-                    Row(
-                        horizontalArrangement = Arrangement.spacedBy(16.dp),
-                        modifier = Modifier.fillMaxWidth()
+                    Text(text = "Stats")
+                    Column(
+                        modifier = Modifier.fillMaxWidth().height(240.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
                     ) {
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(16.dp),
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            StatCard(
+                                "Characters",
+                                textState.length.toString(),
+                                modifier = Modifier.weight(1f)
+                            )
+                            StatCard(
+                                "Words",
+                                getWordsCounting(textState).toString(),
+                                modifier = Modifier.weight(1f)
+                            )
+                        }
                         StatCard(
-                            "Characters",
-                            textState.length.toString(),
-                            modifier = Modifier.weight(1f)
-                        )
-                        StatCard(
-                            "Words",
-                            getWordsCounting(textState).toString(),
+                            "Lines",
+                            getLinesCounting(textState).toString(),
                             modifier = Modifier.weight(1f)
                         )
                     }
-                    StatCard(
-                        "Lines",
-                        getLinesCounting(textState).toString(),
-                        modifier = Modifier.weight(1f)
-                    )
                 }
-            }
-            Button(onClick = { textState = "" }, modifier = Modifier.fillMaxWidth()) {
-                Text("Clear")
+                Button(onClick = { textState = "" }, modifier = Modifier.fillMaxWidth()) {
+                    Text("Clear")
+                }
             }
         }
     }
