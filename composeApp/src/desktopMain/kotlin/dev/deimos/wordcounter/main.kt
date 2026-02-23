b6ddce41683105810093169199e2b97edd8a0aa3
deimos <deimoshall.dev@proton.me>
2026-02-22 20:50:54 -0600

    feat: set default position and window size for desktop

--- composeApp/src/desktopMain/kotlin/dev/deimos/wordcounter/main.kt
+++ composeApp/src/desktopMain/kotlin/dev/deimos/wordcounter/main.kt
@@ -1,12 +1,21 @@
 package dev.deimos.wordcounter
 
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Word Counter",
+        state = rememberWindowState(
+            position = WindowPosition.Aligned(Alignment.Center),
+            size = DpSize(1000.dp, 700.dp)
+        )
     ) {
         App(false)
     }
