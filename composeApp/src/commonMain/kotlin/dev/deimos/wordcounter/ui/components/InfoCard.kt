9c9d14f0592259f1533162f250901f2b6bdc9fd4
deimos <deimoshall.dev@proton.me>
2026-03-12 09:55:38 -0600

    feat: add about screen content

---
+++ composeApp/src/commonMain/kotlin/dev/deimos/wordcounter/ui/components/InfoCard.kt
@@ -0,0 +1,50 @@
+package dev.deimos.wordcounter.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun InfoCard(
+    title: String,
+    contentDescription: String,
+    modifier: Modifier = Modifier
+) {
+    ElevatedCard(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.elevatedCardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+            contentColor = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = contentDescription,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/dev/deimos/wordcounter/ui/screens/AboutScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/deimos/wordcounter/ui/screens/AboutScreen.kt
@@ -1,11 +1,22 @@
 package dev.deimos.wordcounter.ui.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -14,13 +25,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import dev.deimos.wordcounter.ui.components.InfoCard
 import dev.deimos.wordcounter.ui.theme.WordCounterTheme
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import wordcounter.composeapp.generated.resources.Res
 import wordcounter.composeapp.generated.resources.about
+import wordcounter.composeapp.generated.resources.icon
 
 @Composable
 fun AboutScreen(onNavigateBack: () -> Unit) {
@@ -42,12 +60,27 @@ fun AboutScreen(onNavigateBack: () -> Unit) {
             }
             Text(stringResource(Res.string.about))
         }
-
-        Text(
-            text = "About this App",
-            fontSize = 24.sp,
-            modifier = Modifier.align(Alignment.Center)
-        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Spacer(modifier = Modifier.height(64.dp))
+            Image(
+                painterResource(Res.drawable.icon),
+                contentDescription = "",
+                modifier = Modifier.size(120.dp).clip(RoundedCornerShape(16.dp)),
+            )
+            Text(
+                text = "Word Counter",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold
+            )
+            // TODO: wrap in a chip
+            Text(text = "v0.1.0")
+            InfoCard("Author", "Deimos Hall")
+            InfoCard("License", "GPLv3")
+        }
     }
 }
 
