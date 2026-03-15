9138ebaf9bb35f1241c13017cd6208f14e1136b9
deimos <deimoshall.dev@proton.me>
2026-03-15 09:29:17 -0600

    feat: add app icon to main class

--- composeApp/src/desktopMain/kotlin/dev/deimos/wordcounter/main.kt
+++ composeApp/src/desktopMain/kotlin/dev/deimos/wordcounter/main.kt
@@ -7,11 +7,15 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import org.jetbrains.compose.resources.painterResource
+import wordcounter.composeapp.generated.resources.Res
+import wordcounter.composeapp.generated.resources.icon
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Word Counter",
+        icon = painterResource(Res.drawable.icon),
         state = rememberWindowState(
             position = WindowPosition.Aligned(Alignment.Center),
             size = DpSize(1000.dp, 700.dp)
